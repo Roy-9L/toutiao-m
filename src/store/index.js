dfbf7bd27267898d9ab5b5a079f8c795c6ded029
Roy-9L <l>
2019-11-15 08:47:18 +0800

    项目初始化

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,16 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // 申明数据的
   state: {
   },
+  // 修改数据的
   mutations: {
   },
+  // 异步拿数据的
   actions: {
-  },
-  modules: {
   }
+  // modules:模块 当项目非常复杂时，如果全部把数据申明在state中，会非常难以维护，modules也同时拥有state mumations actions这三个功能，可以分模块进行维护
+  // modules: {
+  // }
 })
